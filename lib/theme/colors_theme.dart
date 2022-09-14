f62017bfd834185cea39c43622243db275156bf4
KaueSena01 <kauealvessena17@gmail.com>
2022-09-14 17:11:36 -0300

    feature: Adição da logo no fundo

---
+++ lib/theme/colors_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart'; 
+
+// Constantes
+
+const primaryColor = Color(0xFF0A3470);
+const secondaryColor = Color(0xFF0082F0);
+const ligthColor = Color(0xFFE6E6E6);
+const darkColor = Color(0xFF4A4D4A);
+const depositColor = Color(0xFF368B34);
+const expenseColor = Color(0xFFE03E3E);
--- lib/views/auth/login_screen.dart
+++ lib/views/auth/login_screen.dart
@@ -1,12 +1,74 @@
+import 'package:balanced/theme/colors_theme.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({ Key? key }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      
+  Widget build(BuildContext context) { 
+    return Scaffold(
+      body: Column(
+          children: [
+            Stack(
+              children: [
+                Opacity(
+                  opacity: 1,
+                  child: ClipPath(
+                    clipper: WaveClipper(),
+                    child: Container(
+                      color: secondaryColor,
+                      height: 220,
+                    ),
+                  ),
+                ),
+                Opacity(
+                  opacity: 1,
+                  child: ClipPath(
+                    clipper: WaveClipper(),
+                    child: Container(
+                      color: primaryColor,
+                      height: 200,
+                    ),
+                  ),
+                ), 
+                Positioned(
+                  right: 5,
+                  bottom: 60,
+                  child:
+                  Image.asset(
+                    'assets/images/logo_transparent.png',
+                    height: 125,
+                  )  
+                )
+              ], 
+            ),
+            
+          ],
+      )
     );
+  }
+}
+
+class WaveClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    
+      var path = Path();
+      path.lineTo(0, size.height); 
+
+      var secondEnd = Offset(size.width - (size.width) - 100, size.height - 200);
+
+      var secondStart = Offset(size.width, size.height);  
+
+      path.quadraticBezierTo(secondEnd.dx, secondEnd.dy, secondStart.dx, secondStart.dy);
+
+      path.lineTo(size.width, 0);  
+      path.close();
+      return path; 
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+     return false; 
   }
 }
