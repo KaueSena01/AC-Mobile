cd8a72b4909b03a3bfa194e63d449378ad5adff4
KaueSena01 <kauealvessena17@gmail.com>
2022-11-04 10:13:45 -0300

    feature: Refatoração na tela de perfil e atualização de senha

--- lib/common/input_text_widget.dart
+++ lib/common/input_text_widget.dart
@@ -3,17 +3,23 @@ import 'package:flutter/material.dart';
 
 class InputTextWidget extends StatefulWidget {
 
-  final String placeholder;
+  final String? placeholder;
+  final String? label;
   final TextEditingController? controller;
   final String? Function(String?)? validator;
   final String? initialValue;
+  final bool? hint;
+  final bool readOnly;
 
   const InputTextWidget({
     Key? key,
-    required this.placeholder,
+    this.placeholder,
+    this.label,
     this.controller,
     this.validator,
-    this.initialValue
+    this.initialValue,
+    this.hint = false,
+    this.readOnly = false
   }) : super(key: key);
 
   @override
@@ -23,21 +29,34 @@ class InputTextWidget extends StatefulWidget {
 class _InputTextWidgetState extends State<InputTextWidget> {
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: widget.controller,
-      validator: widget.validator,
-      initialValue: widget.initialValue,
-      decoration: InputDecoration(
-        isDense: false,
-        hintText: widget.placeholder,
-        hintStyle: const TextStyle(color: mainDarkColor, fontSize: 16),
-        border: const UnderlineInputBorder(),
-        focusedBorder: const UnderlineInputBorder(
-          borderSide: BorderSide(color: mainDarkColor, width: 2)),
-          enabledBorder: const UnderlineInputBorder(
-                borderSide: BorderSide(color: mainDarkColor, width: 2)
-        )
-      )
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        if(widget.hint!)
+          Text(widget.label!,
+            style: const TextStyle(
+              color: darkColor,
+              fontSize: 18
+            )
+          ), 
+        TextFormField(
+          controller: widget.controller,
+          validator: widget.validator,
+          initialValue: widget.initialValue,
+          readOnly: widget.readOnly,
+          decoration: InputDecoration(
+            isDense: false,
+            hintText: widget.placeholder,
+            hintStyle: const TextStyle(color: mainDarkColor, fontSize: 16),
+            border: const UnderlineInputBorder(),
+            focusedBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(color: mainDarkColor, width: 2)),
+              enabledBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: mainDarkColor, width: 2)
+            )
+          )
+        ),
+      ],
     );
   }
 }
--- lib/services/utils/validators.dart
+++ lib/services/utils/validators.dart
@@ -6,17 +6,33 @@ String? validateName(name) {
   return null;
 }
 
-String? validateEmail(name) {
-  if(name == null || name.isEmpty) {
+String? validateEmail(email) {
+  if(email == null || email.isEmpty) {
     return "Digite seu e-mail";
   }
 
   return null;
 }
 
-String? validatePassword(name) {
-  if(name == null || name.isEmpty) {
+String? validatePassword(password) {
+  if(password == null || password.isEmpty) {
     return "Digite sua senha";
+  }
+
+  return null;
+}
+
+String? validateNewPassword(newPassword) {
+  if(newPassword == null || newPassword.isEmpty) {
+    return "Digite a nova senha";
+  }
+
+  return null;
+}
+
+String? validatePasswordConfirmation(newPassword) {
+  if(newPassword == null || newPassword.isEmpty) {
+    return "Por favor, confime a senha";
   }
 
   return null;
